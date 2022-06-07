f1769220a516e99c2d48e097f9858ec69cf9770f
Anusree V A <anusreeva93@gmail.com>
2022-06-07 19:54:08 +0530

    Finished drag and drop and started upload feature

--- uploader-ui/src/App.js
+++ uploader-ui/src/App.js
@@ -8,15 +8,31 @@ function App() {
   return (
     <div className="App">
       <div className="main-container">
+        <div className="heading">Upload your image</div>
+        <div className="help-text">Files should be jpeg,png,jpg...</div>
         <div className="drag-drop-container">
           <DragAndDrop onUpload={onUpload} count={1} formats={["jpg", "png"]}>
             <div className="drag-drop-area">
+              <div className="placeholder-container">
+                <img
+                  src="/images/placeholder.png"
+                  alt="placeholder"
+                  className="placeholder-image"
+                ></img>
+              </div>
               Drag and Drop your image here
-              <span role="img" aria-label="emoji" className="area__icon">
+              {/* <span role="img" aria-label="emoji" className="area__icon">
                 &#128526;
-              </span>
+              </span> */}
             </div>
           </DragAndDrop>
+        </div>
+        <div className="or-label">Or</div>
+        <div>
+          <label className="choose-file-button-container choose-file-button">
+            <input type="file" id="choose-file" name="choose-file" />
+            Choose file
+          </label>
         </div>
       </div>
     </div>
