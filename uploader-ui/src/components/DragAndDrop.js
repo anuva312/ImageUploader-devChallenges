fec1462a85e5df5b42d3c8fe23ada8fd08edc7dd
Anusree V A <anusreeva93@gmail.com>
2022-10-10 18:22:53 +0530

    Fixed warnings

--- uploader-ui/src/components/DragAndDrop.js
+++ uploader-ui/src/components/DragAndDrop.js
@@ -18,101 +18,76 @@ export default function DragAndDrop({
     type: null,
   });
 
-  React.useEffect(() => {
-    drop.current.addEventListener("dragover", handleDragOver);
-    drop.current.addEventListener("drop", handleDrop);
-
-    const dragAndDropElement = drop.current;
-
-    return () => {
-      dragAndDropElement.removeEventListener("dragover", handleDragOver);
-      dragAndDropElement.removeEventListener("drop", handleDrop);
-    };
-  }, []);
-
-  React.useEffect(() => {
-    drop.current.addEventListener("dragover", handleDragOver);
-    drop.current.addEventListener("drop", handleDrop);
-    drop.current.addEventListener("dragenter", handleDragEnter);
-    drop.current.addEventListener("dragleave", handleDragLeave);
-
-    const dragAndDropElement = drop.current;
-
-    return () => {
-      dragAndDropElement.removeEventListener("dragover", handleDragOver);
-      dragAndDropElement.removeEventListener("drop", handleDrop);
-      dragAndDropElement.removeEventListener("dragenter", handleDragEnter);
-      dragAndDropElement.removeEventListener("dragleave", handleDragLeave);
-    };
-  }, []);
-
-  const handleDragOver = (e) => {
+  const handleDragOver = React.useCallback((e) => {
     e.preventDefault();
     e.stopPropagation();
-  };
+  }, []);
 
-  const handleDrop = (e) => {
-    e.preventDefault();
-    e.stopPropagation();
+  const handleDrop = React.useCallback(
+    (e) => {
+      e.preventDefault();
+      e.stopPropagation();
 
-    setDragging(false);
+      setDragging(false);
 
-    // this is required to convert FileList object to array
-    const files = [...e.dataTransfer.files];
+      // this is required to convert FileList object to array
+      const files = [...e.dataTransfer.files];
 
-    // check if the provided count prop is less than uploaded count of files
-    if (count && count < files.length) {
-      showMessage("", "error", 2000);
-      handleError(
-        `Sorry, only ${count} file${
-          count !== 1 ? "s" : ""
-        } can be uploaded at a time`
-      );
-      return;
-    }
+      // check if the provided count prop is less than uploaded count of files
+      if (count && count < files.length) {
+        showMessage("", "error", 2000);
+        handleError(
+          `Sorry, only ${count} file${
+            count !== 1 ? "s" : ""
+          } can be uploaded at a time`
+        );
+        return;
+      }
 
-    // check if some uploaded file is not in one of the allowed formats
-    if (
-      formats &&
-      files.some(
-        (file) =>
-          !formats.some((format) =>
-            file.name.toLowerCase().endsWith(format.toLowerCase())
-          )
-      )
-    ) {
-      showMessage("", "error", 2000);
-      handleError(
-        `Sorry, only following file formats are acceptable: ${formats.join(
-          ", "
-        )}`
-      );
-      return;
-    }
+      // check if some uploaded file is not in one of the allowed formats
+      if (
+        formats &&
+        files.some(
+          (file) =>
+            !formats.some((format) =>
+              file.name.toLowerCase().endsWith(format.toLowerCase())
+            )
+        )
+      ) {
+        showMessage("", "error", 2000);
+        handleError(
+          `Sorry, only following file formats are acceptable: ${formats.join(
+            ", "
+          )}`
+        );
+        return;
+      }
 
-    if (files && files.length) {
-      showMessage("", "success", 1000); //Proceeding to upload...
-      onUpload(files);
-    }
-  };
+      if (files && files.length) {
+        showMessage("", "success", 1000); //Proceeding to upload...
+        onUpload(files);
+      }
+    },
+    [count, formats, handleError, onUpload]
+  );
 
-  const handleDragEnter = (e) => {
+  const handleDragEnter = React.useCallback((e) => {
     e.preventDefault();
     e.stopPropagation();
 
     if (e.target !== drag.current) {
       setDragging(true);
     }
-  };
+  }, []);
 
-  const handleDragLeave = (e) => {
+  const handleDragLeave = React.useCallback((e) => {
     e.preventDefault();
     e.stopPropagation();
 
     if (e.target === drag.current) {
       setDragging(false);
     }
-  };
+  }, []);
 
   const showMessage = (text, type, timeout) => {
     setMessage({
@@ -131,6 +106,34 @@ export default function DragAndDrop({
       timeout
     );
   };
+
+  React.useEffect(() => {
+    drop.current.addEventListener("dragover", handleDragOver);
+    drop.current.addEventListener("drop", handleDrop);
+
+    const dragAndDropElement = drop.current;
+
+    return () => {
+      dragAndDropElement.removeEventListener("dragover", handleDragOver);
+      dragAndDropElement.removeEventListener("drop", handleDrop);
+    };
+  }, [handleDragOver, handleDrop]);
+
+  React.useEffect(() => {
+    drop.current.addEventListener("dragover", handleDragOver);
+    drop.current.addEventListener("drop", handleDrop);
+    drop.current.addEventListener("dragenter", handleDragEnter);
+    drop.current.addEventListener("dragleave", handleDragLeave);
+
+    const dragAndDropElement = drop.current;
+
+    return () => {
+      dragAndDropElement.removeEventListener("dragover", handleDragOver);
+      dragAndDropElement.removeEventListener("drop", handleDrop);
+      dragAndDropElement.removeEventListener("dragenter", handleDragEnter);
+      dragAndDropElement.removeEventListener("dragleave", handleDragLeave);
+    };
+  }, [handleDragOver, handleDrop, handleDragEnter, handleDragLeave]);
 
   return (
     <div ref={drop} className="drag-and-drop">
